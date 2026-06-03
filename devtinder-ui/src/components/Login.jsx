acbe34f6e3dd33325f84458593455f2abad59764
shubham6912 <56666844+shubham6912@users.noreply.github.com>
2026-06-03 14:01:59 +0530

    Added signup

--- devtinder-ui/src/components/Login.jsx
+++ devtinder-ui/src/components/Login.jsx
@@ -6,20 +6,23 @@ import { useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
-    const [emailId, setEmailId] = useState("shubhamg@gmail.com");
-    const [password, setPassword] = useState("Aca#c@12333312");
-    const [error , setError] = useState("");
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(true);
+    const [error, setError] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const handleLogin = async () => {
         try {
-            const res = await axios.post(BASE_URL+"/login", {
+            const res = await axios.post(BASE_URL + "/login", {
                 emailId: emailId,
                 password: password
             }, { withCredentials: true });
 
-           
+
             dispatch(addUser(res.data));
             return navigate("/");
         } catch (err) {
@@ -27,16 +30,59 @@ const Login = () => {
         }
     };
 
+    const handleSignup = async () => {
+        try {
+
+            const res = await axios.post(BASE_URL + "/signup",
+                { firstName, lastName, emailId, password },
+                { withCredentials: true }
+            );
+            dispatch(addUser(res.data.data))
+            return navigate("/profile")
+
+        } catch (err) {
+            setError(err?.response?.data || "Something went wrong");
+        }
+    }
+
     return (
         <div className="flex justify-center mt-10">
             <div className="card bg-base-300 w-96 shadow-xl">
                 <div className="card-body">
                     <h2 className="card-title text-2xl justify-center">
-                        Login
+                        {isLoginForm ? "Login" : "Signup"}
                     </h2>
-
                     <div className="mt-4">
+                        {!isLoginForm && (
+                            <>
 
+                                <fieldset className="fieldset mb-5">
+                                    <legend className="fieldset-legend text-lg">
+                                        FirstName
+                                    </legend>
+                                    <input
+                                        type="text"
+                                        value={firstName}
+                                        className="input text-lg w-full"
+                                        placeholder="Enter FirstName"
+                                        onChange={(e) => setFirstName(e.target.value)}
+                                    />
+                                </fieldset>
+                                <fieldset className="fieldset mb-5">
+                                    <legend className="fieldset-legend text-lg">
+                                        LastName
+                                    </legend>
+                                    <input
+                                        type="text"
+                                        value={lastName}
+                                        className="input text-lg w-full"
+                                        placeholder="Enter lastName"
+                                        onChange={(e) => setLastName(e.target.value)}
+                                    />
+                                </fieldset>
+                            </>
+                        )
+                        }
                         <fieldset className="fieldset mb-5">
                             <legend className="fieldset-legend text-lg">
                                 Email
@@ -66,10 +112,14 @@ const Login = () => {
                     </div>
                     <p className='text-red-500'>{error}</p>
                     <div className="card-actions justify-center mt-6">
-                        <button className="btn btn-primary text-lg px-6" onClick={handleLogin}>
-                            Login
+                        <button className="btn btn-primary text-lg px-6" onClick={isLoginForm ? handleLogin : handleSignup}>
+                            {isLoginForm ? "Login" : "Signup"}
                         </button>
                     </div>
+                    <p className="m-auto cursor-pointer py-2" onClick={() => setIsLoginForm((value) => !value)}>
+                        {isLoginForm ? "New User? Signup Here" :
+                            "Existing User ? Login Here"}
+                    </p>
                 </div>
             </div>
         </div>
